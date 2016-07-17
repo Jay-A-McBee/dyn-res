c2a22c78d75364fef3499a178a2de272e5b4e0d9
Jay McBee <jmcbee1@gmail.com>
2016-07-17 15:32:39 -0500

    tweak static path

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 
 var app = express();
@@ -8,7 +9,7 @@ var app = express();
 app.use( bodyParser.urlencoded ({extended:true}) );
 app.use(bodyParser.json());
 
-app.use(express.static('../App'));
+app.use(express.static(path.resolve(__dirname,'../App/dist')));
 
 require('./routes')(app);
 
