463e5ed037ee8361db3a13ed68326e019d0dcfc1
Jay McBee <jmcbee1@gmail.com>
2016-07-18 11:53:33 -0500

    change dev server config

--- Server/index.js
+++ Server/index.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var webpackDevMiddleware = require('webpack-dev-middleware');
-var webpackHotMiddleware = require('webpack-hot-middleware');
 var webpack = require('webpack');
 var config = require('../webpack.config');
 var prodConfig = require('../webpack.config.prod');
 var path = require('path');
 
-var isProduction = process.env.NODE_ENV === 'production';
+var isProduction = process.env.NODE_ENV === 'production'; 
 
 var app = express();
 
 if(!isProduction){
 
   //allows webpack rebuilds on server restarts
+	var webpackDevMiddleware = require('webpack-dev-middleware');
+	var webpackHotMiddleware = require('webpack-hot-middleware');
   var compiler = webpack(config);
   app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
   app.use(webpackHotMiddleware(compiler));
