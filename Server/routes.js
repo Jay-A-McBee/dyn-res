19f246bc910a80770e73efcfb1b2d320691ab243
Jay McBee <jmcbee1@gmail.com>
2016-07-19 11:24:54 -0500

    new bckgr pic, favicon, routes

--- Server/routes.js
+++ Server/routes.js
@@ -4,8 +4,21 @@ var path = require('path');
 
 module.exports = function(app){
 
-	app.get('/', function(req,res){
+	app.get('/*', function(req,res){
 		res.sendFile(path.resolve(__dirname, '../App/dist/index.html'))
 	})
+
+	app.get('/about',function(req,res){
+		res.redirect('/')
+	})
+
+  app.get('/projects',function(req,res){
+		res.redirect('/')
+	})
+
+  app.get('/resume',function(req,res){
+		res.redirect('/')
+	})
+
 
 }
