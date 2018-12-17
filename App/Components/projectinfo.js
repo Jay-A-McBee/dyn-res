ec88f1f00e2b40258ccfd713f7fc61f4a7c2503a
Jay McBee <jmcbee1@gmail.com>
2018-12-16 18:29:04 -0800

    set target to _blank

--- App/Components/projectinfo.js
+++ App/Components/projectinfo.js
@@ -18,7 +18,7 @@ export const ProjectInfo = ({title, role, desc, tasks, photo, func, prop, link, 
 	
 	return(
       <div className = 'mdl-grid'>
-        <div className = 'mdl-cell--4-col'>
+        <div className = 'mdl-cell mdl-cell--4-col'>
           <h2>{title}</h2>
           <h4>{desc}</h4>
           <h4>Role: {role}</h4>
@@ -27,10 +27,10 @@ export const ProjectInfo = ({title, role, desc, tasks, photo, func, prop, link, 
           </ul>
           {link && 
             <div className = 'centerText'>
-              <a href = { link }>View Repo</a>
+              <a target='_blank' href = { link }>View Repo</a>
             </div>}
         </div>
-        <div className = 'mdl-cell--6-col screenshot'>
+        <div className = 'mdl-cell mdl-cell--6-col screenshot'>
           <img className = 'showFast' src = {photo} />
         </div>
       </div>
