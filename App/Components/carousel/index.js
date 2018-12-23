45d8703167970f55bc2229de235614cd7b9782c7
Jay McBee <jmcbee1@gmail.com>
2018-12-22 19:52:51 -0800

    WIP carousel

--- App/Components/carousel/index.js
+++ App/Components/carousel/index.js
@@ -1,23 +1,89 @@
 import React, {useState} from 'react';
 
 
-const carousel = ({children = ['left', 'current', 'right']}) => {
-  let [active, updateActive] = useState(Math.floor(children.length/2));
+const carousel = ({children = ['0', '1', '2', '3', '4', '5']}) => {
+  const centerIdx = Math.floor(children.length/2) - 1;
 
+  let [activeIndexes, update] = useState({ 
+    active: centerIdx,
+    next: centerIdx+1,
+    previous: centerIdx-1 
+  });
 
-  
-  const rotateLeft = () => {
-    const current = document.getElementById('current');
-    const previous = document.getElementById('previous');
-    const next = document.getElementById('next');
-    const {
-      right,
-      width
-    }= getCoords(next);
-    
-    current.className = `${current.className} transitionLeft`;
-    next.style.transform = `translate3d(-${width}px, 0px, 200px)`;
+  const updateActiveIndexes = (activeObj) => {
+    update(activeObj);
   }
+
+  const transitionStyles = (width) => ({
+    becomecurrent: {
+      transform: `translate3d(-${width * .5}px, 0px, 1em)`,
+      boxShadow: "0px 8px 10px gray, -10px 8px 10px gray, 10px 8px 10px gray"
+    },
+    becomePrevious: {
+      transform: `translate3d(-${width * .75}px, 0px, -1em)`,
+      boxShadow: "none"
+    },
+    next: {},
+    leave: {
+      transform: `translate3d(${width * .75}px, 0px, -5em)`,
+    }
+  })
+
+  const {
+    becomecurrent,
+    becomePrevious,
+    next,
+    leave
+  } = transitionStyles(200);
+
+  let [
+    currentStyle, 
+    updateCurrentStyle
+  ] = useState({
+    "boxShadow": "0px 8px 10px gray, -10px 8px 10px gray, 10px 8px 10px gray",
+    "zIndex": "10"
+  });
+
+  const calcCurrentStyle = () => {
+    updateCurrentStyle(becomePrevious);
+  }
+
+  let [
+    nextStyle, 
+    updateNextStyle
+  ] = useState({});
+
+  const calcNextStyle = () => {
+    updateNextStyle(becomecurrent);
+  }
+
+  let [
+    previousStyle, 
+    updatePreviousStyle
+  ] = useState({});
+
+  const calcPrevStyle = () => {
+    updatePreviousStyle({});
+  }
+
+  const rotateLeft = () => {
+    const {
+      active,
+      next,
+      previous
+    } = activeIndexes;
+    
+    [calcCurrentStyle, calcNextStyle, calcPrevStyle].forEach( (fn, idx) => {
+      if(idx === 0) fn();
+      else setTimeout( () => fn(), 1000);
+    });
+    
+    setTimeout( () => updateActiveIndexes({
+      active: next,
+      previous: active,
+      next: next + 1
+    }), 1000);
+  };
 
   function getCoords(el){
     const {
@@ -30,15 +96,25 @@ const carousel = ({children = ['left', 'current', 'right']}) => {
       width
     }
   };
+
   const rotateRight = () => updateActive(active-=1);
 
+  const carouselCard = (val, id, className, style) => {
+    return id ? (
+      <div key={val} id={id} className={`carouselCard ${className}`} style={{...style}}>{val}</div>
+    ) : (
+      <div key={val} className={`carouselCard`} hidden={true}>{val}</div>
+    )
+  };
 
   return(
     <div className='carouselBody'>
       <i className='material-icons' style={{"fontSize": "40px"}} onClick={rotateLeft}>chevron_left</i>
-        <div id = 'previous'className="carouselCard previous" style={{"fontSize": "40px"}}>{children[active-1]}</div>
-        <div id='current' className="carouselCard current" style={{"fontSize": "40px"}}>{children[active]}</div>
-        <div id = 'next' className="carouselCard next" style={{"fontSize": "40px"}}>{children[active+1]}</div>
+        {children.slice(activeIndexes.previous, activeIndexes.next + 1).map( (el, idx) => {
+          if(idx === 0) return carouselCard(el, 'previous', 'previous', previousStyle);
+          if(idx === 1) return carouselCard(el, 'current', '', currentStyle);
+          if(idx === 2) return carouselCard(el, 'next', 'next show', nextStyle);
+        })}
       <i className='material-icons' style={{"fontSize": "40px"}} onClick={rotateRight}>chevron_right</i>
     </div>
   )
