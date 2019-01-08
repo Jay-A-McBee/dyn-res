f031db5ae7177d29c26211269e3c5fdc68594901
Jay McBee <jmcbee1@gmail.com>
2019-01-07 16:26:40 -0800

    icons and styles

--- App/Components/about.js
+++ App/Components/about.js
@@ -7,37 +7,32 @@ export const About = () => {
   const contactStyles = {
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-evenly'
+    justifyContent:'center',
+    marginTop: '2em'
   }
   
 	return (
     <div className = 'about'>
-      <h1 style={{color: 'rgba(122, 203, 168, 0.9)'}}>About</h1>
+      <h1>About</h1>
       <img className = 'pull-left right' src = {Me} />
-      <p className='top' style={{fontSize:'20px', color: 'rgba(122, 203, 168, 0.9)'}}>{bio}</p>
+      <p className='top' style={{fontSize:'1.3em'}}>{bio}</p>
       <div style={{...contactStyles}}>
         <a target='_blank' href='mailto:jmcbee1@gmail.com'>
-          <i 
-            style={{color: 'rgba(122, 203, 168, 0.9)'}}
-            className='pointer material-icons md-48'
+          <i
+            className='zmdi zmdi-google zmdi-hc-2x'
           >
-            google
           </i>
         </a>
-        <a target='_blank' href='https://github.com/Jay-A-McBee'>
-          <i 
-            style={{color: 'rgba(122, 203, 168, 0.9)'}}
-            className='pointer material-icons md-48'
+        <a target='_blank' href='https://github.com/Austin-ClickTripz'>
+          <i
+            className='zmdi zmdi-github-box zmdi-hc-2x'
           >
-            github
           </i>
         </a>
         <a target='_blank' href='https://www.linkedin.com/in/jayaustinmcbee/'>
-          <i 
-            style={{color: 'rgba(122, 203, 168, 0.9)'}}
-            className='pointer material-icons md-48'
+          <i
+            className='zmdi zmdi-linkedin-box zmdi-hc-2x'
           >
-            linkedin-box
           </i>
         </a>
       </div>
