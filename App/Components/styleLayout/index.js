5d82ae77e3fdcae6d96f1c01137160d9bdf60864
Jay McBee <jmcbee1@gmail.com>
2019-01-12 09:59:26 -0800

    fix styles

--- App/Components/styleLayout/index.js
+++ App/Components/styleLayout/index.js
@@ -2,15 +2,15 @@ import styled, {css} from 'styled-components';
 
 export const SectionWrapper = styled.section`
     display: flex;
-    flexDirection: ${props => props.flexDirection || 'column'};
-    justifyContent:${props => props.justify || 'space-between'};
-    marginTop: 2em;
+    flex-direction: ${props => props.flexDirection || 'column'};
+    justify-content:${props => props.justify || 'space-between'};
+    margin-top: 2em;
 `;
 
 export const ContentWrapper = styled.section`
-    flexDirection: ${props => props.flexDirection || 'column'};
-    justifyContent: ${props => props.justify || 'space-between'};
-    alignSelf: ${props => props.alignSelf || 'center'};
+    flex-direction: ${props => props.flexDirection || 'column'};
+    justify-content: ${props => props.justify || 'space-between'};
+    align-self: ${props => props.alignSelf || 'center'};
     width: ${props => props.width || '100%'}
     marginTop: 2em;
 `;
--- App/Components/styledText/index.js
+++ App/Components/styledText/index.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const SectionHeader = styled.h1`
-    fontWeight: '700', 
-    fontSize: '4em', 
-    color: 'white'
+    font-weight: 700;
+    font-size: 4em;
+    color: white;
 `;
