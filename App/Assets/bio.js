bc94a8aa711f7ebc2a7c69691dbc3f512e953d25
Jay McBee <jmcbee1@gmail.com>
2019-02-06 11:06:57 -0800

    tweak bio

--- App/Assets/bio.js
+++ App/Assets/bio.js
@@ -1,6 +1,6 @@
 const textBlock = {};
 
-textBlock.bio = 'I\'m a software developer in Seattle, Washington. I spent most of my twenties as a Peace Corps volunteer and teacher in North, West and East Africa. I met a lot of interesting folks and studied a number of languages. Eventually got hooked on a quirky one--JavaScript. '
+textBlock.bio = 'I spent most of my twenties as a Peace Corps volunteer and teacher in North, West and East Africa. I met a lot of interesting folks and studied a number of languages. Eventually got hooked on a quirky one--JavaScript. '
 
 textBlock.aside = 'Got serious about it in 2015 and joined my first team as a JS engineer. I\'m familiar with the auto-magic of modern frontend frameworks and can also appreciate a little node.js workflow script.'
 
