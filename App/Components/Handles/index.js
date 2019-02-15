074a7e593c121f5870b1216961887542e698b6b1
Jay McBee <jmcbee1@gmail.com>
2019-02-15 14:54:16 -0800

    fix indentation

--- App/Components/Handles/index.js
+++ App/Components/Handles/index.js
@@ -2,24 +2,24 @@ import React, {useImperativeHandle} from 'react';
 
 
 export const scrollImperativeHandle = (container, ref, id) => {
-    return useImperativeHandle(ref, () => ({
-        id,
-        scroll: () => {
-          container.current.scrollIntoView({
-            block: 'start', 
-            inline: 'nearest', 
-            behavior: 'smooth'
-          })
-        },
-        getCoords: () => {
-            const {
-              top
-            } = container.current.getBoundingClientRect();
+  return useImperativeHandle(ref, () => ({
+    id,
+    scroll: () => {
+      container.current.scrollIntoView({
+        block: 'start', 
+        inline: 'nearest', 
+        behavior: 'smooth'
+      })
+    },
+    getCoords: () => {
+      const {
+        top
+      } = container.current.getBoundingClientRect();
 
-            return{
-              top,
-              offset: container.current.offsetTop
-            }
-        } 
-    }));
+      return{
+        top,
+        offset: container.current.offsetTop
+      }
+    } 
+  }));
 }
