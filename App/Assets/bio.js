b641edcbb10b1bfff0d497a596ee897f9f2898a4
Jay McBee <jmcbee1@gmail.com>
2019-03-04 21:50:17 -0800

    copy tweak

--- App/Assets/bio.js
+++ App/Assets/bio.js
@@ -2,4 +2,4 @@ module.exports = textBlock = {};
 
 textBlock.bio = 'I spent most of my twenties as a teacher in North, West and East Africa. I met a lot of interesting folks and studied a number of languages. Eventually got hooked on a quirky one--JavaScript. '
 
-textBlock.aside = 'Got serious about it in 2015 and joined my first team as a software engineer. I have experience with modern, frontend frameworks and although, I\'m no node.js wizard, I have enough server-side JavaScript knowledge to be dangerous.'
+textBlock.aside = 'Got serious about it in 2015 and joined my first team as a software engineer. I have experience with modern, frontend frameworks and I have enough server-side knowledge to be dangerous.'
