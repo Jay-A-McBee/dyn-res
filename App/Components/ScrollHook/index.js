704888c9a4743fc74c08f5a9ac541946c8dc4eb2
Jay McBee <jmcbee1@gmail.com>
2019-03-05 17:33:10 -0800

    swap scroll tracking for intersection observer api/polyfill

--- App/Components/ScrollHook/index.js
+++ App/Components/ScrollHook/index.js
@@ -1,42 +1,35 @@
 import React, {useState, useRef, useEffect, useLayoutEffect} from 'react';
+import io from 'intersection-observer';
 import throttle from 'lodash/throttle';
 
 
 export const useVisibility = (ref) => {
-    let scrollHandler = useRef(); 
+    let observer = useRef(); 
     let [active, setViewState] = useState(false);
 
-
-    useEffect(() => {
-        if(!scrollHandler.current){
-            checkVisibility(ref.current.container.offsetTop);
-            subscribe();
-        }
-
-        if(active) unsubscribe();
-    });
-
-    function checkVisibility(offset){
-      if(!offset) return false;
-      let show = offset - window.scrollY <= 500 || offset < window.innerHeight;
-      if(show){
-        setViewState(show);
-      }
-    }
-
-    function subscribe(){
-        scrollHandler.current = throttle(() => {
-            checkVisibility(ref.current.container.offsetTop);
-        }, 750, {leading:true, trailing: true});
-
-        window.addEventListener('scroll', scrollHandler.current, {passive: true});
+    const config = {
+        root: null,
+        rootMargin: '0px',
+        threshold: .25
     };
 
-    function unsubscribe(){
-        window.removeEventListener('scroll', scrollHandler.current);
-        scrollHandler.current = null;
-    }
+    const setActive = (entries) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) setViewState(true);
+        });
+    };
 
+    useEffect(() => {
+        if(!observer.current){
+            observer.current = window.IntersectionObserver ?
+                new IntersectionObserver(setActive, config) :
+                io(setActive, config);
+        }
+
+        if(ref.current){
+            observer.current.observe(ref.current.container);
+        }
+    });
 
     return active;
 }
