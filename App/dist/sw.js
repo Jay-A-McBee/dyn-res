e582bd08a4845edcc5ec3254b1927cab3e5b884a
Jay-A-McBee <austin.mcbee@formidable.com>
2019-09-14 15:27:12 -0700

    scope fix

--- App/dist/sw.js
+++ App/dist/sw.js
@@ -63,6 +63,21 @@ self.addEventListener('fetch', function(event) {
 });
 
 function update(request) {
+  const matches = [
+    /\/$/,
+    /\/index\.html$/,
+    /\/bundle\.js$/,
+    /\/0\.bundle.js$/,
+    /\/2\.bundle.js$/,
+    /\/3\.bundle.js$/,
+    /\/4\.bundle.js$/,
+    /\/5\.bundle.js$/,
+    /\/6\.bundle.js$/,
+    /\/36deb8949e99886edb7b3a827fbcc0ec.png$/,
+    /\/4a86c0f65974b5f8c30611754b30cdd1.png$/,
+    /\/aa8d3b7e4caef1dd9962b910ab47b02a.jpg$/,
+    /\/favicon.ico$/
+  ];
   if (matches.some(regex => regex.test(request.url))) {
     return caches
       .open('v1')
