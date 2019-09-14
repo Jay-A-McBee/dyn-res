7cd1b59c3eb411b974d3b976397bbf9d86226773
Jay-A-McBee <austin.mcbee@formidable.com>
2019-09-14 15:22:36 -0700

    fix update logic

--- App/dist/sw.js
+++ App/dist/sw.js
@@ -21,6 +21,21 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
+  const matches = [
+    /\/$/,
+    /\/index\.html$/,
+    /\/bundle\.js$/,
+    /\/0\.bundle.js$/,
+    /\/2\.bundle.js$/,
+    /\/3\.bundle.js$/,
+    /\/4\.bundle.js$/,
+    /\/5\.bundle.js$/,
+    /\/6\.bundle.js$/,
+    /\/36deb8949e99886edb7b3a827fbcc0ec.png$/,
+    /\/4a86c0f65974b5f8c30611754b30cdd1.png$/,
+    /\/aa8d3b7e4caef1dd9962b910ab47b02a.jpg$/,
+    /\/favicon.ico$/
+  ];
   event.respondWith(
     caches.match(event.request).then(function(response) {
       // caches.match() always resolves
@@ -30,25 +45,6 @@ self.addEventListener('fetch', function(event) {
       } else {
         return fetch(event.request)
           .then(function(response) {
-            // response may be used only once
-            // we need to save clone to put one copy in cache
-            // and serve second one
-            const matches = [
-              /\/$/,
-              /\/index\.html$/,
-              /\/bundle\.js$/,
-              /\/0\.bundle.js$/,
-              /\/2\.bundle.js$/,
-              /\/3\.bundle.js$/,
-              /\/4\.bundle.js$/,
-              /\/5\.bundle.js$/,
-              /\/6\.bundle.js$/,
-              /\/36deb8949e99886edb7b3a827fbcc0ec.png$/,
-              /\/4a86c0f65974b5f8c30611754b30cdd1.png$/,
-              /\/aa8d3b7e4caef1dd9962b910ab47b02a.jpg$/,
-              /\/favicon.ico$/
-            ];
-
             if (matches.some(regex => regex.test(event.request.url))) {
               let responseClone = response.clone();
               caches.open('v1').then(function(cache) {
@@ -67,14 +63,18 @@ self.addEventListener('fetch', function(event) {
 });
 
 function update(request) {
-  return caches
-    .open('v1')
-    .then(cache => {
-      return fetch(request).then(resp => {
-        return cache.put(request, resp);
+  if (matches.some(regex => regex.test(request.url))) {
+    return caches
+      .open('v1')
+      .then(cache => {
+        return fetch(request).then(resp => {
+          return cache.put(request, resp);
+        });
+      })
+      .catch(e => {
+        console.log('error updating the cache', e);
       });
-    })
-    .catch(e => {
-      console.log('error updating the cache');
-    });
+  }
+
+  return;
 }
