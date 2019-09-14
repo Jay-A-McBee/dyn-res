eb985880d180c592c950988662ef2df5a00a1829
Jay-A-McBee <austin.mcbee@formidable.com>
2019-09-14 14:11:21 -0700

    add /

--- App/dist/sw.js
+++ App/dist/sw.js
@@ -2,6 +2,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
+        '/',
         '/index.html',
         '/bundle.js',
         '/0.bundle.js',
@@ -33,20 +34,22 @@ self.addEventListener('fetch', function(event) {
             // we need to save clone to put one copy in cache
             // and serve second one
             const matches = [
-              '/index.html',
-              '/bundle.js',
-              '/0.bundle.js',
-              '/2.bundle.js',
-              '/3.bundle.js',
-              '/4.bundle.js',
-              '/5.bundle.js',
-              '/6.bundle.js',
-              '/36deb8949e99886edb7b3a827fbcc0ec.png',
-              '/4a86c0f65974b5f8c30611754b30cdd1.png',
-              '/aa8d3b7e4caef1dd9962b910ab47b02a.jpg',
-              '/favicon.ico'
+              /\/$/,
+              /\/index\.html$/,
+              /\/bundle\.js$/,
+              /\/0\.bundle.js$/,
+              /\/2\.bundle.js$/,
+              /\/3\.bundle.js$/,
+              /\/4\.bundle.js$/,
+              /\/5\.bundle.js$/,
+              /\/6\.bundle.js$/,
+              /\/36deb8949e99886edb7b3a827fbcc0ec.png$/,
+              /\/4a86c0f65974b5f8c30611754b30cdd1.png$/,
+              /\/aa8d3b7e4caef1dd9962b910ab47b02a.jpg$/,
+              /\/favicon.ico$/
             ];
-            if (matches.some(url => event.request.url.indexOf(url) !== -1)) {
+
+            if (matches.some(regex => regex.test(event.request.url)) {
               let responseClone = response.clone();
               caches.open('v1').then(function(cache) {
                 cache.put(event.request, responseClone);
