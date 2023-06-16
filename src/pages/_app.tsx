bc0d685e7e26782e87bcf3bd65e00f2b60065bd0
Austin McBee <austin.mcbee@formidable.com>
2023-06-16 12:05:16 -0700

    favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,7 +17,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>J. Austin McBee</title>
         <meta name="description" content="Resume website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚀</text></svg>"
+        />
       </Head>
       <main className={roboto.className}>
         <Component {...pageProps} />
